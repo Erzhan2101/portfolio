f848c86b2bc208155cc7029a9ac0fbc1ab608c89
Erzhan2101 <erzhan210198@gmail.com>
2021-07-01 15:41:32 +0600

    burger-menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+const topMenu = document.querySelector('.menu')
+const  mobileMenu = document.querySelector('.mobile-menu')
+
+mobileMenu.addEventListener('click', () => {
+    topMenu.classList.toggle('show-menu')
+})
+
+const menuLinks = document.querySelectorAll('.menu li a')
+
+menuLinks.forEach(element => element.addEventListener('click', () => {
+    topMenu.classList.remove('show-menu')
+}))
 
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
